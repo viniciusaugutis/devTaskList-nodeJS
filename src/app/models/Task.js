b4882fc74938145537155d36de10a272ae8e5d79
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-18 17:18:32 -0300

    Criando endpoint para listagem de tasks

--- src/app/models/Task.js
+++ src/app/models/Task.js
@@ -16,7 +16,7 @@ class Task extends Model {
   }
 
   static associate(models) {
-    this.hasMany(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
   }
 }
 
