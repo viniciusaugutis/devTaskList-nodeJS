43c66a25b90b26271b650827f6278b75f6e150e9
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-19 11:40:16 -0300

    Criando enviroments de produção e desenvolvimento

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('custom-env').env(process.env.NODE_ENV);
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '123456',
-  database: 'tasklist',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
